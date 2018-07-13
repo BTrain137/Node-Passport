e461ca69549b0527b46f3f24835132dd0b03c921
Bryan Tran <btran@zr-mac-21.local>
2018-07-13 12:23:30 -0700

    add mongoose new parameter

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //NoSQL mongo database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/passport")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/passport", { useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on("error", error => console.log("Database Error:", error));
